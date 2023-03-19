27dcffe008659eae7b967f1418c96fc052b9a854
Vyacheslav Lukin <slava@edgevision.pro>
2023-03-19 22:52:15 +0000

    refactor & add test to get substring at beginning

--- Longest substring/longest_substring.py
+++ Longest substring/longest_substring.py
@@ -2,20 +2,34 @@ class Solution:
 
     def __init__(self, input_string: str) -> None:
         self.str = input_string
-        self.substring = ""
+        self.text_dict = {}
+        self.substring = self.find_substring() if self.str else ""
 
-    def lengthOfLongestSubstring(self) -> int:
+    def length_longest_substring(self) -> int:
         if not self.str:
-            return 0
-
-        dict_text = {}
-        for letter in self.str:
-            if letter in dict_text.keys():
-                dict_text[letter] += 1
-            else:
-                dict_text[letter] = 1
-        
-        return max(dict_text.values())
+            res = 0
+        else: 
+            res = len(self.substring)
+        return res
     
     def get_substring(self) -> str:
         return self.substring
+
+    def find_substring(self) -> bool:
+        def cmp_counter_in_tuple(tup: tuple) -> int:
+            return tup[1]
+
+        self.text_dict = {}
+        for letter in self.str:
+            if letter in self.text_dict.keys():
+                self.text_dict[letter] = (self.text_dict[letter][0] + letter, self.text_dict[letter][1] + 1)
+            else:
+                self.text_dict[letter] = (letter, 1)
+
+        max_key =  max(self.text_dict.values(), key=cmp_counter_in_tuple)
+        substring = max_key[0]
+        return substring
+
+if __name__ == "__main__":
+    s = Solution("aaabb")
+    print(s.length_longest_substring())
--- Longest substring/tests/test.py
+++ Longest substring/tests/test.py
@@ -1,6 +1,5 @@
 import string
 from longest_substring import Solution
-from hypothesis import given, strategies as st
 
 start_st = "aaabb"
 end_st = "aaabbbb"
@@ -8,20 +7,24 @@ mid_st = "aaazzzzzzzzbbbb"
 
 def test_should_return_zero_for_empty_string():
     s = Solution("")
-    assert s.lengthOfLongestSubstring() == 0
+    assert s.length_longest_substring() == 0
 
-def test_should_return_len_substring_at_start():
+def test_should_return_len_substring_at_beginnig():
     s = Solution(start_st)
-    assert s.lengthOfLongestSubstring() == 3 
+    assert s.length_longest_substring() == 3 
 
 def test_should_return_len_substring_at_end():
     s = Solution(end_st)
-    assert s.lengthOfLongestSubstring() == 4
+    assert s.length_longest_substring() == 4
 
 def test_should_return_len_substring_at_middle():
     s = Solution(mid_st)
-    assert s.lengthOfLongestSubstring() == 8
+    assert s.length_longest_substring() == 8
 
 def test_should_return_empty_substring():
     s = Solution("")
     assert s.get_substring() == ""
+
+def test_should_return_substring_at_beginning():
+    s = Solution(start_st)
+    assert s.get_substring() == "aaa"
